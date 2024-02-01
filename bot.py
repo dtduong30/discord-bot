34e0ba0627b6e7794aeadb5c35300d7e3532117b
Duong Dang <dangtuanduong99@gmail.com>
2024-02-01 10:06:17 +0700

    fetch data from api beecost

--- bot.py
+++ bot.py
@@ -27,10 +27,83 @@ async def bye_command(message):
     await message.channel.send(f"Goodbye {message.author}")
 
 
+async def get_info(message):
+    await message.channel.send(f"Goodbye {message.author}")
+
+
+def desired_string(link):
+    start_index = link.find("p.")
+    if start_index == -1:
+        return None  # Return None if "p." is not found in the link
+    start_index += len("p.")
+
+    # Find the end index of the desired string, accounting for "?"
+    end_index = link.find("?")
+    if end_index != -1:
+        desired_string = link[start_index:end_index]
+    else:
+        desired_string = link[
+            start_index:
+        ]  # If "?" is not found, take the substring until the end of the link
+
+    return desired_string
+
+
+async def test(message):
+    # Extract the link from the message content
+    link = message.content.split(" ")[1]
+    print(link)
+    product_base_id = desired_string(link)
+    print(product_base_id)
+    if product_base_id == "":
+        print("Product ID:", product_base_id)
+        await message.channel.send("Failed to fetch data from API")
+        return
+
+    # Call the API to get product details
+    response = requests.get(
+        f"https://apiv3.beecost.vn/product/detail?product_base_id={product_base_id}&type=new"
+    )
+
+    if response.status_code == 200:
+        data = response.json()["data"]["product_base"]
+        # print(data)
+        name = data.get("name", "")
+        price = data.get("price", 0)
+        price_before_discount = data.get("price_before_discount", 0)
+        img = data.get(
+            "url_thumbnail",
+            "https://cdn.discordapp.com/emojis/1113660133877235803.webp?size=128&quality=lossless",
+        )
+        # print(f"Name: {name} \n Price: {price} \n Price before discount: {price_before_discount}")
+        # Format the price to VND currency
+        formatted_price = "{:,.0f} đ".format(price)
+        formatted_price_before_discount = "{:,.0f} đ".format(price_before_discount)
+        # Create an embed with the message content
+        embed = discord.Embed(title="Product Details", color=0x00FF00)  # Green color
+        embed.add_field(name="Name", value=name, inline=False)
+        embed.add_field(name="Price", value=formatted_price, inline=False)
+        embed.add_field(
+            name="Price before discount",
+            value=formatted_price_before_discount,
+            inline=False,
+        )
+        embed.set_image(url=img)
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/emojis/1113660133877235803.webp?size=128&quality=lossless"
+        )
+
+        # Send the response to the Discord channel
+        await message.channel.send(embed=embed)
+    else:
+        await message.channel.send("Failed to fetch data from API")
+
+
 # Command dictionary mapping commands to functions
 command_dict = {
     "hello": hello_command,
     "bye": bye_command,
+    "test": test
     # Add more commands as needed
 }
 
@@ -54,51 +127,6 @@ async def on_message(message):
         if command in command_dict:
             # Call the corresponding function
             await command_dict[command](message)
-
-
-# Start each command with the @bot.command decorater
-@bot.command()
-async def square(ctx, arg):  # The name of the function is the name of the command
-    print(arg)  # this is the text that follows the command
-    await ctx.send(int(arg) ** 2)  # ctx.send sends text in chat
-
-
-@bot.command()
-async def scrabblepoints(ctx, arg):
-    # Key for point values of each letter
-    score = {
-        "a": 1,
-        "c": 3,
-        "b": 3,
-        "e": 1,
-        "d": 2,
-        "g": 2,
-        "f": 4,
-        "i": 1,
-        "h": 4,
-        "k": 5,
-        "j": 8,
-        "m": 3,
-        "l": 1,
-        "o": 1,
-        "n": 1,
-        "q": 10,
-        "p": 3,
-        "s": 1,
-        "r": 1,
-        "u": 1,
-        "t": 1,
-        "w": 4,
-        "v": 4,
-        "y": 4,
-        "x": 8,
-        "z": 10,
-    }
-    points = 0
-    # Sum the points for each letter
-    for c in arg:
-        points += score[c]
-    await ctx.send(points)
 
 
 bot.run(TOKEN)
