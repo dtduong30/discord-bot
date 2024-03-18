7c32d45c7e7b1e89b4ea9bd88d659131fdd897a2
Duong Dang <dangtuanduong99@gmail.com>
2024-03-19 00:10:07 +0700

    Enhance tag user

--- commands/list_cmds.py
+++ commands/list_cmds.py
@@ -1,6 +1,15 @@
 from commands.command import hello_command, bye_command, muoi_buon_command
 from commands.crawl import crawl_bee_cost
-from commands.tag import muoibuon, miss_duong, miss_pam, luv
+from commands.tag import (
+    petting,
+    muoibuon,
+    miss,
+    luv,
+    g9,
+    lick,
+    bc,
+    touch_cheek,
+)
 
 # Add more commands as needed
 command_dict = {
@@ -8,7 +17,11 @@ command_dict = {
     "bye": bye_command,
     "test": crawl_bee_cost,
     "muối_buồn": muoibuon,
-    "miss_duong": miss_duong,
-    "miss_pam": miss_pam,
+    "miss": miss,
     "iu": luv,
+    "g9": g9,
+    "lick": lick,
+    "bc": bc,
+    "cụng_má": touch_cheek,
+    "vuốt_ve": petting,
 }
--- commands/tag.py
+++ commands/tag.py
@@ -4,36 +4,48 @@ agnes = "341107845523046400"
 wakuu = "449456684767641600"  # luckyvippro
 
 
-async def tag_user(user_id, message, thumb, message_channel):
-    mention = f"<@{user_id}>"
-    embed = discord.Embed(description=f"{mention} {message}")
+def get_receiver_id(sender_id):
+    if str(sender_id) == agnes:
+        return wakuu
+    elif str(sender_id) == wakuu:
+        return agnes
+    else:
+        return None
+
+
+async def tag_user(message, msg, thumb, message_channel, is_target=None):
+    sender_id = message.author.id
+    if is_target is not None:
+        receiver_id = is_target
+        sender_mention = ""
+    else:
+        receiver_id = get_receiver_id(sender_id)
+        sender_mention = f"<@{sender_id}>"
+
+    if receiver_id is None:
+        await message.channel.send("You are not authorized to use this command.")
+        return
+    receiver_mention = f"<@{receiver_id}>"
+    embed = discord.Embed(description=f"{sender_mention} nhắn: {msg}")
     embed.set_thumbnail(url=thumb)
     embed.color = discord.Color.purple()
-    await message_channel.send(embed=embed)
+    await message_channel.send(receiver_mention, embed=embed)
 
 
 async def muoibuon(message):
     await tag_user(
-        wakuu,
+        message,
         "Muối buồn ạ!",
         "https://cdn.discordapp.com/emojis/1117366343231082578.gif?size=128&quality=lossless",
         message.channel,
+        wakuu,
     )
 
 
-async def miss_pam(message):
+async def miss(message):
     await tag_user(
-        agnes,
-        "Anh nhớ chị ạ! Thèm quá",
-        "https://cdn.discordapp.com/emojis/1117425666682536056.gif?size=128&quality=lossless",
-        message.channel,
-    )
-
-
-async def miss_duong(message):
-    await tag_user(
-        agnes,
-        "Chị nhớ anh!",
+        message,
+        "nhớ lắm!",
         "https://cdn.discordapp.com/emojis/1117425666682536056.gif?size=128&quality=lossless",
         message.channel,
     )
@@ -41,14 +53,56 @@ async def miss_duong(message):
 
 async def luv(message):
     await tag_user(
-        agnes,
+        message,
         "Love you! Yêu yêu yêu ❤️💕",
         "https://cdn.discordapp.com/emojis/827406624339394571.gif?size=128&quality=lossless",
         message.channel,
     )
+
+
+async def g9(message):
     await tag_user(
-        wakuu,
-        "Love you! Yêu yêu yêu ❤️💕",
+        message,
+        "Ngủ ngon ạ, lớp diu ❤️💕",
         "https://cdn.discordapp.com/emojis/827406624339394571.gif?size=128&quality=lossless",
         message.channel,
+    )
+
+
+async def lick(message):
+    await tag_user(
+        message,
+        "Thèm đc liếm ạ ❤️💕",
+        "https://cdn.discordapp.com/emojis/995765755075379240.gif?size=96&quality=lossless",
+        message.channel,
+    )
+
+
+async def bc(message):
+    await tag_user(
+        message,
+        "Thèm đc bú cu ạ ❤️💕",
+        "https://cdn.discordapp.com/emojis/995765755075379240.gif?size=96&quality=lossless",
+        message.channel,
+        wakuu,
+    )
+
+
+async def touch_cheek(message):
+    await tag_user(
+        message,
+        "Yêu yêu, cụng má ❤️💕",
+        "https://i.pinimg.com/originals/9c/94/78/9c9478bb26b2160733ce0c10a0e10d10.gif",
+        message.channel,
+        agnes,
+    )
+
+
+async def petting(message):
+    await tag_user(
+        message,
+        "Nựng má ❤️💕",
+        "https://i.pinimg.com/originals/8d/82/8c/8d828c5055047d95b2e75b533147f0fd.gif",
+        message.channel,
+        agnes,
     )
